323e5918900acaeb3ab03acaa0d751a149636c41
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2020-09-17 22:19:17 +0300

    Fetch all collections

--- instagrapi/collection.py
+++ instagrapi/collection.py
@@ -5,13 +5,25 @@ class Collection:
     def collections(self) -> list:
         """Return list of collections
         """
-        result = self.private_request(
-            "/collections/list/",
-            params={
-                "collection_types": '["ALL_MEDIA_AUTO_COLLECTION","PRODUCT_AUTO_COLLECTION","MEDIA"]'
-            },
-        )
-        return result["items"]
+        next_max_id = ""
+        total_items = []
+        while True:
+            try:
+                result = self.private_request(
+                    "/collections/list/",
+                    params={
+                        "collection_types": '["ALL_MEDIA_AUTO_COLLECTION","PRODUCT_AUTO_COLLECTION","MEDIA"]',
+                        "max_id": next_max_id
+                    },
+                )
+            except Exception as e:
+                self.logger.exception(e)
+                return total_items
+            total_items.extend(result["items"])
+            if not result.get("more_available"):
+                return total_items
+            next_max_id = result.get("next_max_id", "")
+        return total_items
 
     def collection_medias_by_name(self, name: str) -> list:
         """Helper return medias by collection name
