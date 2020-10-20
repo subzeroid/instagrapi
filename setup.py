10d9e8db8786e8cd50685e7a508e27cb0ef8d9a9
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2020-10-20 19:53:01 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -10,25 +10,25 @@ setup(
     author_email='x11org@gmail.com',
     license='MIT',
     url='https://github.com/adw0rd/instagrapi',
-    install_requires=[
-        'pytz>=2020.1',
-        'requests>=2.24.0',
-        'moviepy>=1.0.3',
-        'Pillow>=7.2.0'
-    ],
+    install_requires=[item.strip() for item in open(
+        os.path.join(base, 'requirements.txt')
+    ).readlines()],
     # test_requires=[],
     keywords='instagram private api',
     description='Fast and effective Instagram Private API wrapper (public+private requests and challenge resolver)',
     long_description=open(os.path.join(base, 'README.md')).read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    python_requires=">=3",
+    python_requires=">=3.6",
     package_data={'': ["requirements.txt"]},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3',
-    ],
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
 )
