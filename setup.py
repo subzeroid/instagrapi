0c1812131c3180b041ba1e82f5b18207a6c3e4a8
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2020-10-21 17:56:53 +0300

    Bump to 1.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ base = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='instagrapi',
-    version='1.1.5',
+    version='1.1.6',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
