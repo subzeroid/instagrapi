771ee1ce771849fd4d5a8e00e3c2bef7c9001735
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2020-12-07 03:15:13 +0300

    Implement Hashtag medias methods [#32] Added hashtag_medias_top and hashtag_medias_recent

--- instagrapi/hashtag.py
+++ instagrapi/hashtag.py
@@ -1,23 +1,34 @@
 import time
+from typing import List
 
-from .extractors import extract_hashtag_gql, extract_hashtag_v1
+from .extractors import (
+    extract_hashtag_gql,
+    extract_hashtag_v1,
+    extract_media_v1
+)
 from .exceptions import ClientError
+from .types import Hashtag, Media
+from .utils import dumps
 
 
 class HashtagMixin:
 
-    def hashtag_info_a1(self, name, max_id=None):
+    def hashtag_info_a1(self, name: str, max_id: str = None) -> Hashtag:
+        """Get info (id, name, profile_pic_url)
+        """
         params = {"max_id": max_id} if max_id else None
         data = self.public_a1_request(
             f"/explore/tags/{name}/", params=params
         )
         return extract_hashtag_gql(data["hashtag"])
 
-    def hashtag_info_gql(self, name, count=12, end_cursor=None):
+    def hashtag_info_gql(self, name: str, amount: int = 12, end_cursor: str = None) -> Hashtag:
+        """Get info (id, name, profile_pic_url)
+        """
         variables = {
             "tag_name": name,
             "show_ranked": False,
-            "first": int(count),
+            "first": int(amount)
         }
         if end_cursor:
             variables["after"] = end_cursor
@@ -26,11 +37,15 @@ class HashtagMixin:
         )
         return extract_hashtag_gql(data["hashtag"])
 
-    def hashtag_info_v1(self, name):
+    def hashtag_info_v1(self, name: str) -> Hashtag:
+        """Get info (id, name, profile_pic_url)
+        """
         result = self.private_request(f'tags/{name}/info/')
         return extract_hashtag_v1(result)
 
-    def hashtag_info(self, name, count=12):
+    def hashtag_info(self, name: str) -> Hashtag:
+        """Get info (id, name, profile_pic_url)
+        """
         try:
             hashtag = self.hashtag_info_a1(name)
         except Exception as e:
@@ -39,29 +54,127 @@ class HashtagMixin:
             hashtag = self.hashtag_info_v1(name)
         return hashtag
 
-    def hashtag_top_feed(self, hashtag):
-        data = self.hashtag_info_a1(hashtag)
-        return data["edge_hashtag_to_top_posts"]["edges"]
-
-    def hashtag_related_hashtags(self, hashtag):
-        data = self.hashtag_info_a1(hashtag)
+    def hashtag_related_hashtags(self, name):
+        """Get related hashtags
+        """
+        data = self.public_a1_request(f"/explore/tags/{name}/")
         return [
-            item["node"]["name"]
-            for item in data["edge_hashtag_to_related_tags"]["edges"]
+            extract_hashtag_gql(item["node"])
+            for item in data['hashtag']['edge_hashtag_to_related_tags']["edges"]
         ]
 
-    def hashtag_feed(self, hashtag, count=70, sleep=2):
+    def hashtag_medias_a1(self, name: str, amount: int = 27, sleep: float = 0.5, tab_key: str = '') -> List[Media]:
+        """Receive medias by hastag name
+        """
         medias = []
         end_cursor = None
         while True:
-            data = self.hashtag_info(hashtag, end_cursor)
-            end_cursor = data["edge_hashtag_to_media"]["page_info"]["end_cursor"]
-            edges = data["edge_hashtag_to_media"]["edges"]
-            medias.extend(edges)
-            if (
-                not data["edge_hashtag_to_media"]["page_info"]["has_next_page"]
-                or len(medias) >= count
-            ):
+            data = self.public_a1_request(
+                f'/explore/tags/{name}/',
+                params={"max_id": end_cursor} if end_cursor else {}
+            )['hashtag']
+            page_info = data["edge_hashtag_to_media"]["page_info"]
+            end_cursor = page_info["end_cursor"]
+            edges = data[tab_key]["edges"]
+            for edge in edges:
+                if amount and len(medias) >= amount:
+                    break
+                node = edge['node']
+                # node haven't video_url and
+                #   User fields (username, pic_url, full_name)
+                # if 'username' not in node['owner']:
+                #     node['owner'] = self.user_short_gql(
+                #         node['owner']['id']
+                #     ).dict()
+                #     time.sleep(sleep)
+                # medias.append(extract_media_gql(node))
+                medias.append(
+                    self.media_info_gql(node['id'])
+                )
+                time.sleep(sleep)
+            if not page_info["has_next_page"] or not end_cursor:
+                break
+            if amount and len(medias) >= amount:
                 break
             time.sleep(sleep)
-        return medias[:count]
+        # Post unique filtration
+        # (if calculate immediately, then the cycle can be infinite)
+        uniq_pks = set()
+        medias = [
+            m for m in medias
+            if not (m.pk in uniq_pks or uniq_pks.add(m.pk))
+        ]
+        if amount:
+            medias = medias[:amount]
+        return medias
+
+    def hashtag_medias_v1(self, name: str, amount: int = 27, tab_key: str = '') -> List[Media]:
+        """Receive medias by hastag name
+        """
+        data = {
+            'supported_tabs': dumps([tab_key]),
+            # 'lat': 59.8626416,
+            # 'lng': 30.5126682,
+            'include_persistent': 'true',
+            'rank_token': self.rank_token,
+        }
+        result = self.private_request(
+            f'tags/{name}/sections/', self.with_default_data(data)
+        )
+        medias = []
+        for section in result['sections']:
+            for node in section['layout_content']['medias']:
+                medias.append(
+                    extract_media_v1(node['media'])
+                )
+        if amount:
+            medias = medias[:amount]
+        return medias
+
+    def hashtag_medias_top_a1(self, name: str, amount: int = 9, sleep: float = 0.5) -> List[Media]:
+        """Top medias
+        """
+        return self.hashtag_medias_a1(
+            name, amount, sleep=sleep,
+            tab_key='edge_hashtag_to_top_posts'
+        )
+
+    def hashtag_medias_top_v1(self, name: str, amount: int = 9) -> List[Media]:
+        """Top medias
+        """
+        return self.hashtag_medias_v1(name, amount, tab_key='top')
+
+    def hashtag_medias_top(self, name: str, amount: int = 9) -> List[Media]:
+        """Top medias
+        """
+        try:
+            medias = self.hashtag_medias_top_a1(name, amount)
+        except Exception as e:
+            if not isinstance(e, ClientError):
+                self.logger.exception(e)
+            medias = self.hashtag_medias_top_v1(name, amount)
+        return medias
+
+    def hashtag_medias_recent_a1(self, name: str, amount: int = 27, sleep: float = 0.5) -> List[Media]:
+        """Recent medias
+        """
+        return self.hashtag_medias_a1(
+            name, amount, sleep=sleep,
+            tab_key='edge_hashtag_to_media'
+        )
+
+    def hashtag_medias_recent_v1(self, name: str, amount: int = 27) -> List[Media]:
+        """All medias
+        """
+        return self.hashtag_medias_v1(name, amount, tab_key='recent')
+
+    def hashtag_medias_recent(self, name: str, amount: int = 27) -> List[Media]:
+        """All medias
+        """
+        try:
+            medias = self.hashtag_medias_recent_a1(name, amount)
+        except Exception as e:
+            if not isinstance(e, ClientError):
+                self.logger.exception(e)
+            medias = self.hashtag_medias_recent_v1(name, amount)
+        return medias
--- tests.py
+++ tests.py
@@ -19,7 +19,6 @@ from instagrapi.types import (
 ACCOUNT_USERNAME = os.environ.get("IG_USERNAME", "instagrapi2")
 ACCOUNT_PASSWORD = os.environ.get("IG_PASSWORD", "yoa5af6deeRujeec")
 
-
 REQUIRED_MEDIA_FIELDS = [
     "pk", "taken_at", "id", "media_type", "code", "thumbnail_url", "location",
     "user", "comment_count", "like_count", "caption_text", "usertags",
@@ -870,6 +869,11 @@ class ClientLocationTestCase(ClientPrivateTestCase):
 
 
 class ClientHashtagTestCase(ClientPrivateTestCase):
+    REQUIRED_MEDIA_FIELDS = [
+        "pk", "taken_at", "id", "media_type", "code", "thumbnail_url",
+        "like_count", "caption_text", "video_url", "view_count",
+        "video_duration", "title"
+    ]
 
     def test_hashtag_info(self):
         hashtag = self.api.hashtag_info('dhbastards')
@@ -884,6 +888,54 @@ class ClientHashtagTestCase(ClientPrivateTestCase):
         self.assertEqual('dhbastards', hashtag_a1.name)
         self.assertEqual(hashtag_a1.id, hashtag_v1.id)
         self.assertEqual(hashtag_a1.name, hashtag_v1.name)
+        self.assertEqual(hashtag_a1.media_count, hashtag_v1.media_count)
+
+    def test_hashtag_medias_top(self):
+        medias = self.api.hashtag_medias_top('dhbastards', amount=2)
+        self.assertEqual(len(medias), 2)
+        self.assertIsInstance(medias[0], Media)
+
+    def test_extract_hashtag_medias_top(self):
+        medias_a1 = self.api.hashtag_medias_top_a1('dhbastards', amount=9)
+        medias_v1 = self.api.hashtag_medias_top_v1('dhbastards', amount=9)
+        self.assertEqual(len(medias_a1), 9)
+        self.assertIsInstance(medias_a1[0], Media)
+        self.assertEqual(len(medias_v1), 9)
+        self.assertIsInstance(medias_v1[0], Media)
+
+    def test_hashtag_medias_recent(self):
+        medias = self.api.hashtag_medias_recent('dhbastards', amount=2)
+        self.assertEqual(len(medias), 2)
+        self.assertIsInstance(medias[0], Media)
+
+    def test_extract_hashtag_medias_recent(self):
+        medias_a1 = self.api.hashtag_medias_recent_a1('dhbastards', amount=11)
+        medias_v1 = self.api.hashtag_medias_recent_v1('dhbastards', amount=11)
+        self.assertEqual(len(medias_a1), 11)
+        self.assertIsInstance(medias_a1[0], Media)
+        self.assertEqual(len(medias_v1), 11)
+        self.assertIsInstance(medias_v1[0], Media)
+        for i, a1 in enumerate(medias_a1):
+            a1 = a1.dict()
+            v1 = medias_v1[i].dict()
+            for f in self.REQUIRED_MEDIA_FIELDS:
+                a1_val, v1_val = a1[f], v1[f]
+                is_album = a1['media_type'] == 8
+                is_video = v1.get('video_duration') > 0
+                if f == 'thumbnail_url' and not is_album:
+                    a1_val = a1[f].path.rsplit('/', 1)[1]
+                    v1_val = v1[f].path.rsplit('/', 1)[1]
+                if f == 'video_url' and is_video:
+                    a1_val = a1[f].path.rsplit('.', 1)[1]
+                    v1_val = v1[f].path.rsplit('.', 1)[1]
+                if f in ('view_count', 'like_count'):
+                    # instagram can different counts for public and private
+                    if f == 'view_count' and not is_video:
+                        continue
+                    self.assertTrue(a1_val > 1)
+                    self.assertTrue(v1_val > 1)
+                    continue
+                self.assertEqual(a1_val, v1_val)
 
 
 if __name__ == '__main__':
