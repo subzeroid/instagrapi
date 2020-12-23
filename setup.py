bbb76c8ccda59828ff6f3c1dd600faf7aa052be6
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2020-12-23 23:36:14 +0300

    Bump 1.3.2 with new description

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
 
 long_description = '''
-Fast and effective Instagram Private API wrapper (public+private requests and challenge resolver)
+Fast and effective Instagram Private API wrapper (public+private requests and challenge resolver).
+
+Use the most recent version of the API from Instagram.
+
+### Features
+
+1. Performs Public API (web, anonymous) or Private API (mobile app, authorized) requests depending on the situation (to avoid Instagram limits)
+2. Challenge Resolver have Email (as well as recipes for automating receive a code from email) and SMS handlers
+3. Support upload a Photo, Video, IGTV, Albums and Stories
+4. Support work with User, Media, Insights, Collections, Location (Place), Hashtag and Direct objects
+5. Like, Follow, Edit account (Bio) and much more else
+6. Insights by account, posts and stories
+7. Build stories with custom background, font animation, swipe up link and mention users
+8. In the next release, account registration and captcha passing will appear
 '''
 
 setup(
     name='instagrapi',
-    version='1.3.1',
+    version='1.3.2',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
