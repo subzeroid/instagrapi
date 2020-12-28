edbf926283eb6af2a8aece7014a976d913b4221d
Jash Parekh <jparekh1@wayfair.com>
2020-12-27 23:03:21 -0500

    Remove Bandit

--- docker/lint.sh
+++ docker/lint.sh
@@ -4,8 +4,5 @@ echo "Running flake8..."
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 
-echo "Running bandit..."
-bandit -r instagrapi
-
 echo "Running isort..."
 isort instagrapi
