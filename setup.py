d58c719f8b34cff0bf2ef3287e680cd9be668579
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2020-12-30 02:39:24 +0300

    Bump 1.3.9

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.3.8',
+    version='1.3.9',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
