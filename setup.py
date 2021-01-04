32d247919ce317bdc616bd65ca7d5deffb01fdde
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-01-04 17:06:10 +0300

    Bump 1.4.1 (save/unsave collection)

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.4.0',
+    version='1.4.1',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
