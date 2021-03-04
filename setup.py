2cba80224a14fbbfc1a11f44afca6a40bed2cc58
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-03-04 20:51:52 +0300

    Bump 1.5.1 (Redesigned login for Instagram 169.3.0.30.135)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.5.0',
+    version='1.5.1',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
