a71cf6e138c9139cf026aad42b38adcf9a1d936b
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-03-04 18:28:42 +0300

    PasswordMixin.password_encrypt fixed

--- instagrapi/mixins/password.py
+++ instagrapi/mixins/password.py
@@ -10,23 +10,24 @@ class PasswordMixin:
     def password_encrypt(self, password):
         publickeyid, publickey = self.password_publickeys()
         session_key = get_random_bytes(32)
-        iv = bytearray(12)
+        iv = get_random_bytes(12)
         timestamp = datetime.now().strftime('%s')
         decoded_publickey = base64.b64decode(publickey.encode())
         recipient_key = RSA.import_key(decoded_publickey)
         cipher_rsa = PKCS1_v1_5.new(recipient_key)
-        enc_session_key = cipher_rsa.encrypt(session_key)
+        rsa_encrypted = cipher_rsa.encrypt(session_key)
         cipher_aes = AES.new(session_key, AES.MODE_GCM, iv)
         cipher_aes.update(timestamp.encode())
-        ciphertext, tag = cipher_aes.encrypt_and_digest(password.encode("utf8"))
+        aes_encrypted, tag = cipher_aes.encrypt_and_digest(password.encode("utf8"))
+        size_buffer = len(rsa_encrypted).to_bytes(2, byteorder='little')
         payload = base64.b64encode(b''.join([
-            b"\x01\x00",
-            publickeyid.to_bytes(2, byteorder='big'),
+            b"\x01",
+            publickeyid.to_bytes(1, byteorder='big'),
             iv,
-            len(enc_session_key).to_bytes(2, byteorder='big'),
-            enc_session_key,
+            size_buffer,
+            rsa_encrypted,
             tag,
-            ciphertext
+            aes_encrypted
         ]))
         return f"#PWD_INSTAGRAM:4:{timestamp}:{payload.decode()}"
 
