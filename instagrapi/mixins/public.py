a50b1ee27051917b4ae94fb38ef7479298c0f0e9
alexey <you@example.com>
2021-03-29 21:45:17 +0300

    int(response.headers.get("Content-Length", 0))

--- instagrapi/mixins/public.py
+++ instagrapi/mixins/public.py
@@ -102,7 +102,7 @@ class PublicRequestMixin:
             else:  # GET
                 response = self.public.get(url, params=params)
 
-            expected_length = int(response.headers.get("Content-Length"))
+            expected_length = int(response.headers.get("Content-Length", 0))
             actual_length = response.raw.tell()
             if actual_length < expected_length:
                 raise ClientIncompleteReadError(
