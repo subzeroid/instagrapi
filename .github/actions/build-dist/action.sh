cd50b9642b702ac00a118f903d33574d7ba43ef9
Jash Parekh <jparekh1@wayfair.com>
2021-04-17 08:05:13 -0400

    Remove verify wheel step

--- .github/actions/build-dist/action.sh
+++
@@ -1,8 +0,0 @@
-set -euo pipefail
-
-echo "Ensuring pip is up to date"
-python -m pip install --upgrade pip
-echo "Installing the latest version of pypa/build"
-pip install build
-
-python -m build --sdist --wheel --outdir dist/ .
