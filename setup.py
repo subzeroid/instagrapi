a3106f36223b35403dfdceb1a8ca6afe1ca91e1f
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-05-11 19:52:08 +0200

    Bump 1.6.3 (Fixed TooManyRedirects and 429 Client Error: Please wait a few minutes before you try again)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.6.2',
+    version='1.6.3',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
