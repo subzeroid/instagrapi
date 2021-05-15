93338c95ff592391917b355789e4b00a634cb644
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-05-16 01:33:47 +0300

    Bump 1.7.2 (media_archive, media_unarchive, direct_thread_hide and comment_bulk_delete)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.7.1',
+    version='1.7.2',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
