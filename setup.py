0878c3eab18798ed2947b2662a35d578695d2289
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-05-19 18:35:49 +0300

    Bump 1.7.4 (fix StoryBuilder and sessionid)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.7.3',
+    version='1.7.4',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
