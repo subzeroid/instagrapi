ab490405b772998a7c58997529daf31b5ac32098
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-08-18 21:01:42 +0200

    Bump 1.10.0 (BloksMixin, bloks_versioning_id, manual_change_password)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.9.19',
+    version='1.10.0',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
