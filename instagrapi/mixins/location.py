82f20e8e87d111ef4f8557b1c2a8db92fca77746
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-08-20 21:49:44 +0200

    Fix isort requirements

--- instagrapi/mixins/location.py
+++ instagrapi/mixins/location.py
@@ -2,14 +2,9 @@ import json
 import time
 from typing import List, Tuple
 
-from instagrapi.exceptions import (
-    ClientError,
-    ClientNotFoundError,
-    LocationNotFound,
-)
+from instagrapi.exceptions import ClientError, ClientNotFoundError, LocationNotFound
 from instagrapi.extractors import extract_location, extract_media_v1
 from instagrapi.types import Location, Media
-
 
 tab_keys_a1 = ('edge_location_to_top_posts', 'edge_location_to_media')
 tab_keys_v1 = ('ranked', 'recent')
