6649a79fb62c3f93cee604e1b371f61efba77399
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-09-22 21:20:01 +0200

    Implement unread filter for direct threads [#380] Added selected_filter ("flagged", "unread") for cl.direct_threads

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.13.4',
+    version='1.13.5',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
