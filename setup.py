4bd27ce7eecf7dd0c29d11bd1297e137050f6a02
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-09-28 19:02:05 +0200

    Implement getting highlights [#215] Added HighlightMixin (highlight_info and highlight_pk_from_url)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.13.6',
+    version='1.14.0',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
