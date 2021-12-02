5989e37c762f00bd2aaef1d3570cb7cf784d95d8
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2021-12-02 08:29:23 +0100

    No "response_to_comment" method [#365] Added replied_to_comment_id for cl.media_comment

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.15.19',
+    version='1.15.20',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
