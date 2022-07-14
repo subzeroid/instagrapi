7c4112134bda6514323046a98fcb1c3daebb9e62
Trevor White <trevor.white@wayne.edu>
2022-07-14 12:09:09 -0400

    Added an optional random_delay before every public_request and private_request
    - delay_range is now an optional variable that can be set in the client class
    - if a delay_range is set, random_delay() is called before EVERY public_request() and private_request()

--- instagrapi/utils.py
+++ instagrapi/utils.py
@@ -107,3 +107,9 @@ def generate_jazoest(symbols: str) -> str:
 def date_time_original(localtime):
     # return time.strftime("%Y:%m:%d+%H:%M:%S", localtime)
     return time.strftime("%Y%m%dT%H%M%S.000Z", localtime)
+
+
+def random_delay(delay_range: list):
+    """Trigger sleep of a random floating number in range min_sleep to max_sleep
+    """
+    return time.sleep(random.uniform(delay_range[0], delay_range[1]))
