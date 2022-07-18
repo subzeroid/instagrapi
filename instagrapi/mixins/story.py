53dc06b7a39b84c4afdc38f2a046997ae49ed971
Mikhail <x11org@gmail.com>
2022-07-18 15:11:30 +0200

    Story downloader returns invalid media_id error [#765] New story_info_v1

--- instagrapi/mixins/story.py
+++ instagrapi/mixins/story.py
@@ -62,9 +62,12 @@ class StoryMixin:
         """
         story_id = self.media_id(story_pk)
         story_pk, user_id = story_id.split("_")
-        result = self.private_request(f"media/{story_pk}/info/")
-        story = extract_story_v1(result["items"][0])
-        self._stories_cache[story.pk] = story
+        # result = self.private_request(f"media/{story_pk}/info/")
+        # story = extract_story_v1(result["items"][0])
+        stories = self.user_stories_v1(user_id)
+        for story in stories:
+            self._stories_cache[story.pk] = story
+        story = self._stories_cache[story_pk]
         return deepcopy(story)
 
     def story_info(self, story_pk: str, use_cache: bool = True) -> Story:
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='instagrapi',
-    version='1.16.22',
+    version='1.16.23',
     author='Mikhail Andreev',
     author_email='x11org@gmail.com',
     license='MIT',
