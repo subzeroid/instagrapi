682bf7e6906038aa8c662682fd5a31088288e9ce
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2023-02-15 16:29:21 +0300

    Bump 1.16.41

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ Use the most recent version of the API from Instagram.
 
 Features:
 
-1. Performs Public API (web, anonymous) or Private API (mobile app, authorized) requests depending on the situation (to avoid Instagram limits)
+1. Performs Public API (web, anonymous) or Private API (mobile app, authorized)
+   requests depending on the situation (to avoid Instagram limits)
 2. Challenge Resolver have Email (as well as recipes for automating receive a code from email) and SMS handlers
 3. Support upload a Photo, Video, IGTV, Clips (Reels), Albums and Stories
 4. Support work with User, Media, Insights, Collections, Location (Place), Hashtag and Direct objects
@@ -30,7 +31,7 @@ requirements = [
 
 setup(
     name="instagrapi",
-    version="1.16.40",
+    version="1.16.41",
     author="Mikhail Andreev",
     author_email="x11org@gmail.com",
     license="MIT",
