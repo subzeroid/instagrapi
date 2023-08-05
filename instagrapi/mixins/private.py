721ac78ae5a9a66ba757b6ddb763d33a39c6421b
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2023-08-06 01:26:18 +0300

    Fixed extra message for BadPassword

--- instagrapi/mixins/private.py
+++ instagrapi/mixins/private.py
@@ -405,10 +405,12 @@ class PrivateRequestMixin:
                     raise RateLimitError(**last_json)
                 elif error_type == "bad_password":
                     msg = last_json.get("message", "").strip()
-                    if msg and not msg.endswith("."):
-                        msg = "%s. " % msg
+                    if msg:
+                        if not msg.endswith("."):
+                            msg = "%s." % msg
+                        msg = "%s " % msg
                     last_json["message"] = (
-                        "%sIf you are sure that the password is correct, then change your IP address,"
+                        "%sIf you are sure that the password is correct, then change your IP address, "
                         "because it is added to the blacklist of the Instagram Server"
                     ) % msg
                     raise BadPassword(**last_json)
