7dec831470f31eb2eb1401b8b8e39ef8407d050a
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2023-12-31 01:53:27 +0900

    code convention

--- instagrapi/extractors.py
+++ instagrapi/extractors.py
@@ -321,10 +321,10 @@ def extract_direct_message(data):
     if "voice_media" in data:
         if "media" in data["voice_media"]:
             data["media"] = extract_direct_media(data["voice_media"]["media"])
-    if 'timestamp' in data:
-        if type(data['timestamp']) is not datetime:
-            timestamp_int = int(data['timestamp']) / 1000000
-            data['timestamp'] = datetime.fromtimestamp(timestamp_int)
+    if "timestamp" in data:
+        if type(data["timestamp"]) is not datetime:
+            timestamp_int = int(data["timestamp"]) / 1000000
+            data["timestamp"] = datetime.fromtimestamp(timestamp_int)
     clip = data.get("clip", {})
     if clip:
         if "clip" in clip:
