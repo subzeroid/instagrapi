34c40373d57c69cd0d39a599624d22c8244aabcf
Daniel Zeitoun <daniel.zeitoun@finances.gouv.fr>
2024-02-07 11:25:10 +0100

    increase version number

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requirements = [
 
 setup(
     name="instagrapi",
-    version="2.0.4",
+    version="2.1.0",
     author="Mark Subzeroid",
     author_email="143403577+subzeroid@users.noreply.github.com",
     license="MIT",
