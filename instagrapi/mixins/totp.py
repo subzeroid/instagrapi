e3b21201f378d1ef2d1006c3c00473c6477705e9
AngelDev06 <aggelos.tserkeza@gmail.com>
2025-07-26 00:58:14 +0300

    fixed totp utility

--- instagrapi/mixins/totp.py
+++ instagrapi/mixins/totp.py
@@ -129,7 +129,8 @@ class TOTPMixin:
         )
         return result["status"] == "ok"
 
-    def totp_generate_code(self, seed: str) -> str:
+    @staticmethod
+    def totp_generate_code(seed: str) -> str:
         """
         Generate 2FA TOTP code
 
